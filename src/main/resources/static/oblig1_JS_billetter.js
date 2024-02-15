7982db402b8d1bdf98aa1d927fc4221881e09e0a
Leon Zoltan Hjulstad <87582888+leonzhj0@users.noreply.github.com>
2024-02-15 14:17:02 +0100

    Inputvalidering og feilmelding

--- src/main/resources/static/oblig1_JS_billetter.js
+++ src/main/resources/static/oblig1_JS_billetter.js
@@ -1,6 +1,6 @@
 $(function(){
     let billetter = [];
-
+    let validering = true;
 
     $('#kjopBilett').click(function (){
         $('#tabellBilletter').css('display', 'inline-block');
@@ -13,35 +13,67 @@ $(function(){
         let telefnrInput = $('#telefnr').val();
         let epostInput = $('#epost').val();
 
-        const billettKjop = {
-            film: filmInput,
-            antall: antallInput,
-            fornavn: fornavnInput,
-            etternavn: etternavnInput,
-            telefonnr: telefnrInput,
-            epost: epostInput
-        };
-        billetter.push(billettKjop);
+        $('.validering').text('');
+        validering = true;
 
-        ut += "<tr><td>" + billettKjop.film + "</td><td>" + billettKjop.antall + "</td><td>" +
-            billettKjop.fornavn + "</td><td>" + billettKjop.etternavn + "</td><td>" + billettKjop.telefonnr
-            + "</td><td>" + billettKjop.epost + "</td></tr>";
+        if (filmInput == 'fyllTekst'){
+            $('#filmValidering').text('Velg en film!');
+            validering = false;
+        }
+        if (antallInput == null || antallInput == 0){
+            $('#antallValidering').text('Skriv inn et tall større enn null.');
+            validering = false;
+        }
+        let regNavn = /^[a-zA-ZæøåÆØÅ]+$/;
+        if (regNavn.test(fornavnInput) === false || fornavnInput.trim() === ''){
+            $('#fornavnValidering').text('Må skrive inn fornavnet ditt.');
+            validering = false;
+        }
+        if (regNavn.test(etternavnInput) === false || etternavnInput.trim() === ''){
+            $('#etternavnValidering').text('Må skrive inn etternavnet ditt.');
+            validering = false;
+        }
+        const telefnrRegex = /^(\+\d{1,3})?[-.\s]?\d{1,3}([-.\s]?\d{2,3}){1,3}$/;
+        if (!telefnrRegex.test(telefnrInput)) {
+            $('#telefnrValidering').text('Skriv inn et gyldig tlfnr.');
+            validering = false;
+        }
+        const epostRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+        if (!epostRegex.test(epostInput)){
+            $('#epostValidering').text('Skriv inn gyldig epost.');
+            validering = false;
+        }
 
-        $('#tabellBilletter').append(ut);
+        if (validering) {
+            const billettKjop = {
+                film: filmInput,
+                antall: antallInput,
+                fornavn: fornavnInput,
+                etternavn: etternavnInput,
+                telefonnr: telefnrInput,
+                epost: epostInput
+            };
+            billetter.push(billettKjop);
 
-        $('#filmer').val('fyllTekst');
-        $('#antallBilletter').val('');
-        $('#fornavn').val('');
-        $('#etternavn').val('');
-        $('#telefnr').val('');
-        $('#epost').val('');
+            ut += "<tr><td>" + billettKjop.film + "</td><td>" + billettKjop.antall + "</td><td>" +
+                billettKjop.fornavn + "</td><td>" + billettKjop.etternavn + "</td><td>" + billettKjop.telefonnr
+                + "</td><td>" + billettKjop.epost + "</td></tr>";
+            $('#tabellBilletter').append(ut);
+
+            // Clear input fields
+            $('#filmer').val('fyllTekst');
+            $('#antallBilletter').val('');
+            $('#fornavn').val('');
+            $('#etternavn').val('');
+            $('#telefnr').val('');
+            $('#epost').val('');
+        }
     });
 
     $('#slettAlle').click(function (){
         billetter = [];
         $('#tabellBilletter tbody tr:not(:first-child)').empty();
         $('#tabellBilletter').css('display', 'none');
-
         console.log(billetter);
     });
 });
