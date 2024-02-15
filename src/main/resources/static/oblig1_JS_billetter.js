c98386324e46a0fee16d294faef82c0842992753
Leon Zoltan Hjulstad <87582888+leonzhj0@users.noreply.github.com>
2024-02-15 10:12:30 +0100

    JS variabler

---
+++ src/main/resources/static/oblig1_JS_billetter.js
@@ -0,0 +1,8 @@
+$('#formBilletter').addEventListener('submit', function (){
+    let filmInput = $('#filmer');
+    let antallInput = $('#antallBilletter');
+    let fornavnInput = $('#fornavn');
+    let etternavnInput = $('#etternavn');
+    let telefnrInput = $('#telefnr');
+    let epostInput = $('#epost');
+})
