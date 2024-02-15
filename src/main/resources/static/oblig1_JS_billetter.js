0c2a1cdbe43194d50e49b07d5e1f6d2e611f2a58
Leon Zoltan Hjulstad <87582888+leonzhj0@users.noreply.github.com>
2024-02-15 12:51:10 +0100

    Kjøpbillett onclick som tar inn alle inputene og putter dem inn i et array, blanker feltene når knappen trykkes. Slett alle knapp også implementert

--- src/main/resources/static/oblig1_JS_billetter.js
+++ src/main/resources/static/oblig1_JS_billetter.js
@@ -1,8 +1,47 @@
-$('#formBilletter').addEventListener('submit', function (){
-    let filmInput = $('#filmer');
-    let antallInput = $('#antallBilletter');
-    let fornavnInput = $('#fornavn');
-    let etternavnInput = $('#etternavn');
-    let telefnrInput = $('#telefnr');
-    let epostInput = $('#epost');
-})
+$(function(){
+    let billetter = [];
+
+
+    $('#kjopBilett').click(function (){
+        $('#tabellBilletter').css('display', 'inline-block');
+
+        let ut;
+        let filmInput = $('#filmer').val();
+        let antallInput = $('#antallBilletter').val();
+        let fornavnInput = $('#fornavn').val();
+        let etternavnInput = $('#etternavn').val();
+        let telefnrInput = $('#telefnr').val();
+        let epostInput = $('#epost').val();
+
+        const billettKjop = {
+            film: filmInput,
+            antall: antallInput,
+            fornavn: fornavnInput,
+            etternavn: etternavnInput,
+            telefonnr: telefnrInput,
+            epost: epostInput
+        };
+        billetter.push(billettKjop);
+
+        ut += "<tr><td>" + billettKjop.film + "</td><td>" + billettKjop.antall + "</td><td>" +
+            billettKjop.fornavn + "</td><td>" + billettKjop.etternavn + "</td><td>" + billettKjop.telefonnr
+            + "</td><td>" + billettKjop.epost + "</td></tr>";
+
+        $('#tabellBilletter').append(ut);
+
+        $('#filmer').val('fyllTekst');
+        $('#antallBilletter').val('');
+        $('#fornavn').val('');
+        $('#etternavn').val('');
+        $('#telefnr').val('');
+        $('#epost').val('');
+    });
+
+    $('#slettAlle').click(function (){
+        billetter = [];
+        $('#tabellBilletter tbody tr:not(:first-child)').empty();
+        $('#tabellBilletter').css('display', 'none');
+
+        console.log(billetter);
+    });
+});
