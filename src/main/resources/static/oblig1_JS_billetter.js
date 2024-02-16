098d81a01ddf633e299134e1d793c3dd800febbc
Leon Zoltan Hjulstad <87582888+leonzhj0@users.noreply.github.com>
2024-02-16 18:17:58 +0100

    Finishing touches

--- src/main/resources/static/oblig1_JS_billetter.js
+++ src/main/resources/static/oblig1_JS_billetter.js
@@ -1,10 +1,11 @@
 $(function(){
-    let billetter = [];
+    let billetter = []; //Lager array
     let validering = true;
 
     $('#kjopBilett').click(function (){
-        $('#tabellBilletter').css('display', 'inline-block');
+        $('#tabellBilletter').show(); //Viser tabellheader når kjøp billett knappen trykkes
 
+        //Definerer verdier brukt senere
         let ut;
         let filmInput = $('#filmer').val();
         let antallInput = $('#antallBilletter').val();
@@ -13,38 +14,43 @@ $(function(){
         let telefnrInput = $('#telefnr').val();
         let epostInput = $('#epost').val();
 
-        $('.validering').text('');
-        validering = true;
+        $('.validering').text(''); //Tømmer div-boksene med feilmeldinger
+        validering = true; // Reseter validering
 
+        //Sjekker om brukeren har valgt noe annet en "default option"
         if (filmInput == 'fyllTekst'){
             $('#filmValidering').text('Velg en film!');
             validering = false;
         }
+        //Sjekker om bruker har fylt inn et tall, større enn null
         if (antallInput == null || antallInput == 0){
             $('#antallValidering').text('Skriv inn et tall større enn null.');
             validering = false;
         }
-        let regNavn = /^[a-zA-ZæøåÆØÅ]+$/;
-        if (regNavn.test(fornavnInput) === false || fornavnInput.trim() === ''){
+        let regNavn = /^[a-zA-ZæøåÆØÅ]+$/; //Regex for navn med æøå
+        if (regNavn.test(fornavnInput) === false || fornavnInput.trim() === ''){ //tester regex mot input
             $('#fornavnValidering').text('Må skrive inn fornavnet ditt.');
             validering = false;
-        }
+        }//Trimmer whitespace for å sjekke om input er tom i fornavn og etternavn
         if (regNavn.test(etternavnInput) === false || etternavnInput.trim() === ''){
             $('#etternavnValidering').text('Må skrive inn etternavnet ditt.');
             validering = false;
         }
+        //Regex for telefonnr, ulike antall sifre med landskode om bruker ønsker.
         const telefnrRegex = /^(\+\d{1,3})?[-.\s]?\d{1,3}([-.\s]?\d{2,3}){1,3}$/;
         if (!telefnrRegex.test(telefnrInput)) {
             $('#telefnrValidering').text('Skriv inn et gyldig tlfnr.');
             validering = false;
         }
+        //regex for epost, tillatter ikke norske bokstaver: æøå.
         const epostRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
         if (!epostRegex.test(epostInput)){
             $('#epostValidering').text('Skriv inn gyldig epost.');
             validering = false;
         }
-
+        //Om validering er true kjøres dette
         if (validering) {
+            //Lager et objekt som tar inn alle inputs og setter det inn i arrayet definert tidligere
             const billettKjop = {
                 film: filmInput,
                 antall: antallInput,
@@ -55,12 +61,13 @@ $(function(){
             };
             billetter.push(billettKjop);
 
+            //Definerer utskriften og appender den til html tabellen
             ut += "<tr><td>" + billettKjop.film + "</td><td>" + billettKjop.antall + "</td><td>" +
                 billettKjop.fornavn + "</td><td>" + billettKjop.etternavn + "</td><td>" + billettKjop.telefonnr
                 + "</td><td>" + billettKjop.epost + "</td></tr>";
             $('#tabellBilletter').append(ut);
 
-            // Clear input fields
+            //Reseter input til slutt slik at kunde kan skrive inn nye verdier
             $('#filmer').val('fyllTekst');
             $('#antallBilletter').val('');
             $('#fornavn').val('');
@@ -71,9 +78,12 @@ $(function(){
     });
 
     $('#slettAlle').click(function (){
+        //Tømmer arrayet billetter ved å lage et nytt som heter det samme bare uten noen verdier
         billetter = [];
+        //Fjerner alle rader inni tbody unntatt den første som er table header
         $('#tabellBilletter tbody tr:not(:first-child)').empty();
-        $('#tabellBilletter').css('display', 'none');
-        console.log(billetter);
+        //Skjuler tableheader
+        $('#tabellBilletter').hide();
+        //console.log(billetter);
     });
 });
